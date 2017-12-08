ba442727ca3a1b84ec818fb08584044134d91e29
Neville Li <neville@spotify.com>
2017-12-07 20:10:21 -0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jar -tf bazel-bin/generate_workspace/generate_workspace_deploy.jar
+bazel build //generate_workspace:generate_workspace_deploy.jar
 cp bazel-bin/generate_workspace/generate_workspace_deploy.jar transitive_maven_jar
